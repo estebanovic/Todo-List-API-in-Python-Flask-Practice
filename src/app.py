e3bafa094770683b088251ac2b957e7bdd5298dd
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-30 20:07:58 +0000

    progreso

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
+
+
 app = Flask(__name__)
 
 # These two lines should always be at the end of your app.py file.
@@ -11,6 +14,19 @@ todos = [
 @app.route('/todos', methods=['GET'])
 def hello_world():
     return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decode_object = json.loads(request_body)
+    todos.append(decode_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
